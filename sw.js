24402eba228b91912f0aa2256fe0ee1efcd1e4bb
tavarespl <frugakrzysztof@gmail.com>
2026-06-25 08:59:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Ten Service Worker odinstalowuje się i usuwa cały cache
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
+});
